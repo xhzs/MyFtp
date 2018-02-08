324bf8d91c127aa9b47b972a6584ceecf0cf6b43
xhzs <sillyc233@163.com>
2018-02-08 23:28:11 +0800

    add server/main.cpp

---
+++ server/main.cpp
@@ -0,0 +1,18 @@
+/*
+*   Copyright (C) 2018 Sangfor Ltd. All rights reserved.
+*   
+*   Filename：main.cpp
+*   Author  ：xhzs
+*   Create  ：2018年02月08日
+*   Function：
+*   Refer   ：
+*/
+
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("MyFtp server running..");
+	return 0;
+}
